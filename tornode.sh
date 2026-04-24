4cb4a8f4b2dfb5384bb1e2529cde9fd7f1ae55aa
techroy23 <villamor.leroy@gmail.com>
2026-04-25 07:13:35 +0800

    Add useragent and fallback to wget in tornode.sh

--- tornode.sh
+++ tornode.sh
@@ -2,6 +2,14 @@
 set -e
 
 URL="https://raw.githubusercontent.com/maycon/tor-nodes-list/refs/heads/main/exit-nodes.json"
-TOP_N="${1:-6}"
+TOP_N="${1:-10}"
 
-curl -s "$URL" | tr ',' '\n' | grep '"country"' | cut -d'"' -f4 | sort | uniq -c | sort -rn | head -n "$TOP_N" | awk '{printf "%s%s", (NR>1?",":""), $2} END {print ""}'
+fetch() {
+    curl -sL -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36" "$URL" 2>/dev/null | \
+        tr ',' '\n' | grep '"country"' | cut -d'"' -f4 | sort | uniq -c | sort -rn | head -n "$TOP_N" | \
+        awk '{printf "%s%s", (NR>1?",":""), $2} END {print ""}'
+}
+
+fetch || wget -qO- "$URL" 2>/dev/null | \
+    tr ',' '\n' | grep '"country"' | cut -d'"' -f4 | sort | uniq -c | sort -rn | head -n "$TOP_N" | \
+    awk '{printf "%s%s", (NR>1?",":""), $2} END {print ""}'
